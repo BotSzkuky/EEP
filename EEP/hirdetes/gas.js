591e21c5a9652c11cdff878e4355d46cb7d2bf75
BotSzkuky <145557447+BotSzkuky@users.noreply.github.com>
2024-04-30 12:40:41 +0200

    Add files via upload

---
+++ EEP/hirdetes/gas.js
@@ -0,0 +1,75 @@
+const jobs = [
+    {
+        title: "Gázvezeték kiépítés",
+        image: "img/icons8-gas-100.png",
+        location: "Esztergom",
+        details:
+            "Lakásomba szeretnék gázvezetéket kiépíteni a konyhában és a fűtési rendszerhez. Fontos számomra a biztonság, a minőségi munkavégzés és a szakértői tanácsadás az egész folyamat során.",
+        requirements: "Várom az önök ajánlatait és ötleteit a projekt sikeressé tételéhez! Köszönöm!",
+        contact: "Telefonszám: "
+    }
+];
+
+const jobsHeading = document.querySelector(".jobs-list-container h2");
+const jobsContainer = document.querySelector(".jobs-list-container .jobs");
+const jobSearch = document.querySelector(".jobs-list-container .job-search");
+
+let searchTerm = "";
+
+jobsHeading.innerHTML = `${jobs.length} gázszerelő munka elérhető`;
+
+const createJobListingCards = () => {
+    jobsContainer.innerHTML = "";
+
+    jobs.forEach((job) => {
+        if (job.title.toLowerCase().includes(searchTerm.toLowerCase())) {
+            let jobCard = document.createElement("div");
+            jobCard.classList.add("job");
+
+            let image = document.createElement("img");
+            image.src = job.image;
+
+            let title = document.createElement("h3");
+            title.innerHTML = job.title;
+            title.classList.add("job-title");
+
+            let location = document.createElement("div");
+            location.innerHTML = job.location;
+            location.classList.add("location");
+
+            let details = document.createElement("div");
+            details.innerHTML = job.details;
+            details.classList.add("details");
+
+            let requirements = document.createElement("div");
+            requirements.innerHTML = job.requirements;
+            requirements.classList.add("requirements");
+
+            let contact = document.createElement("div");
+            contact.innerHTML = job.contact;
+            contact.classList.add("contact");
+
+            let deleteButton = document.createElement("button");
+            deleteButton.innerText = "Törlés";
+            deleteButton.classList.add("deleteButton");
+
+            jobCard.appendChild(image);
+            jobCard.appendChild(title);
+            jobCard.appendChild(location);
+            jobCard.appendChild(details);
+            jobCard.appendChild(requirements);
+            jobCard.appendChild(contact);
+            jobCard.appendChild(deleteButton);
+
+            jobsContainer.appendChild(jobCard);
+        }
+    });
+};
+
+createJobListingCards();
+
+jobSearch.addEventListener("input", (e) => {
+    searchTerm = e.target.value;
+
+    createJobListingCards();
+});
---
+++ EEP/php/config.php
@@ -0,0 +1,5 @@
+<?php 
+ 
+ $con = mysqli_connect("localhost","root","","eliteep") or die("Sikertelen csatlakozás!");
+
+?>
